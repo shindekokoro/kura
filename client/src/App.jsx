094bfb5bd098e32b682dd739b7c352985d590382
Brian Whisler <brian.whisler@gmail.com>
2024-01-20 21:08:18 -0700

    add ability to add multiple loans to compare.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react';
+import { LoanForm } from './components';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [loanCount, setLoanCount] = useState(1);
+  console.log(loanCount);
+  let forms = [];
+  for (let i = 0; i < loanCount; i++) {
+    forms.push(<LoanForm key={i} />);
+  }
 
   return (
     <>
-      
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              Loan and Interest Calculators
+            </Typography>
+            <Button color="inherit" onClick={() => setLoanCount(loanCount + 1)}>
+              Add Loan
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {forms}
     </>
   );
 }
