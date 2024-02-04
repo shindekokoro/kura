56ce6a2f285c6d0862273c9d9fa80b63ccd3d01b
Brian Whisler <brian.whisler@gmail.com>
2024-02-04 10:29:33 -0700

    Add kanji to title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 
 function App() {
   const [loanCount, setLoanCount] = useState(1);
-  console.log(loanCount);
+
   let forms = [];
   for (let i = 0; i < loanCount; i++) {
     forms.push(<LoanForm key={i} />);
@@ -16,7 +16,7 @@ function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-              Loan and Interest Calculators
+              (幾ら) Loan and Interest Calculators
             </Typography>
             <Button color="inherit" onClick={() => setLoanCount(loanCount + 1)}>
               Add Loan
