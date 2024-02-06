0d69b28331ca959c147bf0e0e55612d28880ed2d
Brian Whisler <brian.whisler@gmail.com>
2024-02-06 06:32:28 -0700

    Updated payment information if user makes changes

--- client/src/components/PaymentInfo.jsx
+++ client/src/components/PaymentInfo.jsx
@@ -1,29 +1,62 @@
 import { Box, Typography } from '@mui/material';
 
-export default function Payment({ paymentInfo }) {
+export default function Payment({ formState }) {
+  if (
+    !formState.monthlyPayment ||
+    !formState.totalInterest ||
+    !formState.totalCost
+  )
+    return null;
+
+  let updated =
+    formState.updatedTotalInterest.toFixed(2) !==
+    formState.totalInterest.toFixed(2);
   return (
     <Box>
-      <Typography>
-        Monthly Payment:{' '}
-        {paymentInfo.monthlyPayment.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD'
-        })}
-      </Typography>
-      <Typography>
-        Total Interest:{' '}
-        {paymentInfo.totalInterest.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD'
-        })}
-      </Typography>
-      <Typography>
-        Total Cost:{' '}
-        {paymentInfo.totalCost.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD'
-        })}
-      </Typography>
+      <Box sx={{ float: 'left', pr: 8 }}>
+        <Typography>
+          Monthly Payment:{' '}
+          {formState.monthlyPayment.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          })}
+        </Typography>
+        <Typography>
+          Total Interest:{' '}
+          {formState.totalInterest.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          })}
+        </Typography>
+        <Typography>
+          Total Cost:{' '}
+          {formState.totalCost.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          })}
+        </Typography>
+      </Box>
+      {updated ? (
+        <Box sx={{ float: 'left' }}>
+          <Typography>Updated Terms: {formState.updatedPeriods}</Typography>
+          <Typography>
+            Updated Total Interest:{' '}
+            {formState.updatedTotalInterest.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}
+          </Typography>
+          <Typography>
+            Updated Total Cost:{' '}
+            {formState.updatedTotalCost.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}
+          </Typography>
+        </Box>
+      ) : (
+        <></>
+      )}
     </Box>
   );
 }
